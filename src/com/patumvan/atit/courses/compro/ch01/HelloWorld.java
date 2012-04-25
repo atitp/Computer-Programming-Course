58664fb1e56ec1a8c865957444f7d5aa01ee6d3b
Atit Patumvan <atit@patumvan.com>
2012-04-25 11:51:06 +0700

    Add simple hello world class

---
+++ src/com/patumvan/atit/courses/compro/ch01/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.patumvan.atit.courses.compro.ch01;
+
+/**
+ *
+ * @author atitp
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, World!!");
+    }
+}
