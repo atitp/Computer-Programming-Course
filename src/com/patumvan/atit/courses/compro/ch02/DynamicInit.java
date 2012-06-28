9b628b80967df534e402fa833c88588993e02269
Atit Patumvan <atit@patumvan.com>
2012-06-28 14:15:14 +0700

    Add ch02.DynamicInit.java

---
+++ src/com/patumvan/atit/courses/compro/ch02/DynamicInit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.patumvan.atit.courses.compro.ch02;
+
+/**
+ *
+ * @author atitp
+ */
+class DynamicInit {
+	public static void main(String[] args) {
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		System.out.println(num1+num2);
+	}
+}
